a3e20746f37fb3537eac61d5af78631403cfe0bb
emi <emiliorizzo@gmail.com>
2022-07-11 20:24:43 +0000

    Update preview

--- preview/vite.config.js
+++ preview/vite.config.js
@@ -1,14 +1,21 @@
 import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+const hash = Date.now() + Math.floor(Math.random() * 10000)
 
+// https://vitejs.dev/config/
 const config = {
   plugins: [vue()],
   build: {
     sourcemap: false, // enable for debugging
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
+      }
+    }
   },
   resolve: {
     alias: {
