348c7481010c12f370fcba0223ac9b9caae69230
emi <emiliorizzo@gmail.com>
2022-07-12 00:50:26 +0000

    Fix parseMetadata

--- src/metadata.js
+++ src/metadata.js
@@ -2,18 +2,13 @@ import yaml from 'yaml'
 import { calculateTotalRisk } from './Findings'
 
 
-const parseMetadata = (str) => {
-  try {
-    return yaml.parse(str)
-  } catch (err) {
-    return {}
-  }
-}
+const parseMetadata = (str) => yaml.parse(str)
 
 const validateMetadata = (metadata, type) => {
+  if (typeof metadata !== 'object') return metadata
   switch (type) {
     case 'finding':
-      metadata = Object.assign({ ...metadata }, calculateTotalRisk(metadata))
+      metadata = Object.assign(metadata, calculateTotalRisk(metadata))
       break
   }
   return metadata
