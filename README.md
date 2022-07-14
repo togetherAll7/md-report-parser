e45f3cf7541a59a0da5732d66d4fe3b7d3f87189
emi <emiliorizzo@gmail.com>
2022-07-14 20:48:06 +0000

    Move preview to a new repo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # md-report-parser
 Markdown parser for security reports
 
-[demo](https://coinspect.github.io/md-report-parser/)
+[demo](https://coinspect.github.io/md-report-parser-preview/)
--- preview/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-npm run build
-git commit -am "Save uncommited changes (WIP)"
-cd ..
-git branch --delete --force gh-pages
-git checkout --orphan gh-pages
-git add -f preview/dist
-git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter ./preview/dist && git push -f origin gh-pages && git checkout -
