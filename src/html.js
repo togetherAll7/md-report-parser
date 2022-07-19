e5e154cc95685e3257f4a944d779da0218dbead9
emi <emiliorizzo@gmail.com>
2022-07-18 23:19:40 -0300

    Update html, add data-value

--- src/html.js
+++ src/html.js
@@ -1,9 +1,15 @@
 
+
+const getFieldAttributes = (name, value) => { return { class: `field-${name}` } }
+
 export const tag = (t, content, attrs) => {
   if (Array.isArray(content)) content = content.join('\n')
+  if (typeof content === 'boolean') content = `${content}`
   content = content || ''
   if (typeof content !== 'string') throw new Error('Content should be a string')
-  const a = attrs ? Object.entries(attrs).map(([n, v]) => `${n}="${v}"`).join(' ') : ''
+  let a = attrs ? Object.entries(attrs).map(([n, v]) => `${n}="${v}"`).join(' ') : ''
+  if (content.trim().split(' ').length === 1) a += ` data-value="${content.trim()}"`
   return `<${t} ${a}>${content}</${t}>`
 }
-export const dl = (data, attrs) => tag('dl', Object.entries(data).map(([name, value]) => `${tag('dt', name)} ${tag('dd', value)}`), attrs)
+export const dl = (data, attrs) => tag('dl', Object.entries(data)
+  .map(([name, value]) => tag('div', `${tag('dt', name)} ${tag('dd', value)}`, getFieldAttributes(name, value))), attrs)
