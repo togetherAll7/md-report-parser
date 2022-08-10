13dc1f54f4975d74a67ef88bdafc1e955b350c29
emi <emiliorizzo@gmail.com>
2022-08-10 18:54:56 +0000

    Change MdParser methods
    - move parse() to mdParse()
    - replace parse() with MdToObj()

--- src/MdParser.ts
+++ src/MdParser.ts
@@ -3,24 +3,33 @@ import markdown_it_highlightjs from 'markdown-it-highlightjs'
 import markdown_it_anchor from 'markdown-it-anchor'
 import markdown_it_table_of_contents from 'markdown-it-table-of-contents'
 import { solidity } from 'highlightjs-solidity'
-import { default as data_blocks, parseOptions } from 'markdown-it-data-blocks'
+import { default as data_blocks } from 'markdown-it-data-blocks'
 import { MetadataParser } from './metadata'
 import RenderReports from './renderReports'
+import { getOptions, mdDocToMd, MdDoc, createMdBlock } from './mdModel'
+import { MdToObj } from './MdToObj'
 
-type MdParser = {
-  parse: Function
+export type MdParserDef = {
+  mdParse: Function
   render: Function
+  parse: Function
+  toMd: Function
 }
 
-const { metadataBlockTypeName } = parseOptions()
+export type MdParserOptions = MarkdownIt.Options & {
+  debug?: any
+  metadataCb?: any
+}
+
+const { metadataBlockTypeName } = getOptions()
+
 /* eslint-disable @typescript-eslint/naming-convention */
-export function MdParser(
-  options: MarkdownIt.Options & { debug?: any; metadataCb?: any } = {}
-): MdParser {
+export function MdParser(options: MdParserOptions = {}): MdParserDef {
   const { debug, metadataCb } = options
   const metadataParser = MetadataParser({ metadataCb })
 
-  const markdown = new MarkdownIt(options)
+  // Markdown-it instance for rendering
+  const renderer = new MarkdownIt(options)
     .use(data_blocks, {
       ...RenderReports({ metadataBlockTypeName }),
       metadataParser,
@@ -29,11 +38,18 @@ export function MdParser(
     .use(markdown_it_highlightjs, { register: { solidity } })
     .use(markdown_it_anchor)
     .use(markdown_it_table_of_contents, { includeLevel: [2, 3, 4, 5, 6] })
-  const render = (src: string) => markdown.render(src)
-  const parse = (src: string) => {
-    return markdown.parse(src, {})
+
+  const parse = MdToObj(options)
+
+  const render = (src: string) => renderer.render(src)
+
+  const mdParse = (src: string) => {
+    return renderer.parse(src, {})
   }
-  return Object.freeze({ parse, render })
+
+  const toMd = (data: MdDoc) => mdDocToMd(data)
+
+  return Object.freeze({ mdParse, render, parse, toMd })
 }
 
 export default MdParser
--- src/test/mdParser.test.ts
+++ src/test/mdParser.test.ts
@@ -1,18 +1,29 @@
 import { MdParser } from '../MdParser'
+import { isMdDoc } from '../mdModel'
 
 const parser = MdParser()
 const src = '# Test'
 
 describe('MdParser', function () {
-  it('MdParser should have a "parse" method', () => {
-    expect(typeof parser.parse).toEqual('function')
+  it('MdParser should have a "mdParse" method', () => {
+    expect(typeof parser.mdParse).toEqual('function')
   })
+
   it('MdParser should have a "render" method', () => {
     expect(typeof parser.render).toEqual('function')
   })
-  describe('parse', () => {
-    it('Minimal parser test', () => {
-      const result = parser.parse(src)
+
+  it('MdParser should have a "parse" method', () => {
+    expect(typeof parser.parse).toEqual('function')
+  })
+
+  it('MdParser should have a "toMd" method', () => {
+    expect(typeof parser.toMd).toEqual('function')
+  })
+
+  describe('mdParse', () => {
+    it('Minimal mdParser test', () => {
+      const result = parser.mdParse(src)
       expect(Array.isArray(result)).toBe(true)
     })
   })
@@ -21,6 +32,18 @@ describe('MdParser', function () {
       expect(parser.render(src).trim()).toEqual(
         '<h1 id="test" tabindex="-1">Test</h1>'.trim()
       )
+    })
+  })
+
+  describe('parse', () => {
+    it('Minimal  test', () => {
+      expect(isMdDoc(parser.parse(src))).toBe(true)
+    })
+  })
+
+  describe('toMd', () => {
+    it('Minimal  test', () => {
+      expect(parser.toMd(parser.parse(src))).toBe(src)
     })
   })
 })
