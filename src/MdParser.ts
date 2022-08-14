16f41904ba3f4d89e6a1deccbcf7ea18709f1d8c
emi <emiliorizzo@gmail.com>
2022-08-14 01:27:13 +0000

    Update MdParser, add editMd()

--- src/MdParser.ts
+++ src/MdParser.ts
@@ -6,22 +6,25 @@ import { solidity } from 'highlightjs-solidity'
 import { default as data_blocks } from 'markdown-it-data-blocks'
 import { MetadataParser } from './metadata'
 import RenderReports from './renderReports'
-import { getOptions, mdDocToMd, MdDoc, createMdBlock } from './mdModel'
+import { getOptions, mdDocToMd, MdDoc } from './mdModel'
 import { MdToObj } from './MdToObj'
+import { FINDING_TITLE_LEVEL } from './constants'
 
 export type MdParserDef = {
   mdParse: Function
   render: Function
   parse: Function
   toMd: Function
+  editMd: Function
 }
 
 export type MdParserOptions = MarkdownIt.Options & {
-  debug?: any
-  metadataCb?: any
+  debug?: boolean | undefined
+  metadataCb?: Function | undefined
 }
 
 const { metadataBlockTypeName } = getOptions()
+const titleLevel = FINDING_TITLE_LEVEL
 
 /* eslint-disable @typescript-eslint/naming-convention */
 export function MdParser(options: MdParserOptions = {}): MdParserDef {
@@ -33,7 +36,8 @@ export function MdParser(options: MdParserOptions = {}): MdParserDef {
     .use(data_blocks, {
       ...RenderReports({ metadataBlockTypeName }),
       metadataParser,
-      debug
+      debug,
+      titleLevel
     })
     .use(markdown_it_highlightjs, { register: { solidity } })
     .use(markdown_it_anchor)
@@ -49,7 +53,15 @@ export function MdParser(options: MdParserOptions = {}): MdParserDef {
 
   const toMd = (data: MdDoc) => mdDocToMd(data)
 
-  return Object.freeze({ mdParse, render, parse, toMd })
+  const editMd = (md: string, cbs: Function[]): string => {
+    let doc = parse(md)
+    cbs.forEach((cb) => {
+      doc = cb(doc)
+    })
+    return toMd(doc)
+  }
+
+  return Object.freeze({ mdParse, render, parse, toMd, editMd })
 }
 
 export default MdParser
